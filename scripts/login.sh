90f4253cb622ed7d83134ac519fb62c200becb70
Joel Sahleen <joel@sahleen.net>
2021-08-19 11:09:59 -0600

    Fix login shell

--- scripts/login.sh
+++ scripts/login.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
+echo "User email:"
+read email
+echo "User password:"
+read -s password
+
 export JWT=$(curl -H -X POST 'localhost:3000/auth' \
 --header 'Content-Type: application/json' \
 --data-raw '{
-    "password":"'$2'",
-    "email":"'$1'"
+    "email":"'$email'",
+    "password":"'$password'"
 }')
-
-echo $JWT
 
 CLDR_ACCESS_TOKEN=$(echo $JWT | python3 -c "import sys, json; print(json.load(sys.stdin)['accessToken'])")
 CLDR_REFRESH_TOKEN=$(echo $JWT | python3 -c "import sys, json; print(json.load(sys.stdin)['refreshToken'])")
+
+echo "Logged in as ${email}."
